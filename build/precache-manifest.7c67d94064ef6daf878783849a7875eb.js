17e89420850bc33f5fb6379f069a8d15aaefb26e
saroj kumar <send4saroj@gmail.com>
2019-10-13 12:54:25 +0530

    new

---
+++ build/precache-manifest.7c67d94064ef6daf878783849a7875eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff66ba42f24874a08b3c84995074c34a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59b283dc7e7661fc837b",
+    "url": "/static/css/main.6cb51618.chunk.css"
+  },
+  {
+    "revision": "006a3a6e1f08f7668054",
+    "url": "/static/js/2.343fe2bc.chunk.js"
+  },
+  {
+    "revision": "59b283dc7e7661fc837b",
+    "url": "/static/js/main.0f415b17.chunk.js"
+  },
+  {
+    "revision": "1ff96a5fac50e642364f",
+    "url": "/static/js/runtime~main.abd66cfd.js"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/static/media/loder.75ae2ba2.gif"
+  }
+]);
